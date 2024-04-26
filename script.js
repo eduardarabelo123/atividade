b8332388d21bff3319f0ca95de85d63b9ea3a0bd
eduardarabelo123 <eduardadasilvarabelo@gmail.com>
2024-04-26 09:57:57 -0500

    atividade

---
+++ script.js
@@ -0,0 +1,38 @@
+const dom = 1;
+const seg = 2;
+const ter = 3;
+const qua = 4;
+const qui = 5;
+const sex = 6;
+const sab = 7;
+
+let dia = 2;
+
+if (dia ==1){
+document.write("domingo dia 'de dormi' ");
+
+}
+else if (dia ==2){
+    document.write("Segunda dia de etudar");
+
+}
+else if (dia ==3){
+    document.write("terça dia de ir pro curso");
+
+}
+else if (dia ==4){
+    document.write("quarta dia de assisti filme");
+
+}
+else if (dia ==5){
+    document.write("quinta dia de sai");
+
+}
+else if (dia ==6){
+    document.write("sexta dia de ir na casa da sogra");
+
+}
+else if (dia ==7){
+    document.write("sabado dia de lazer");
+
+}
